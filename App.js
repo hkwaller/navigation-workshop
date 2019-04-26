236821291521251a5dbb24e80a6c79e4ba990d58
Hannes Waller <hkwaller@gmail.com>
2019-04-26 15:33:19 +0200

    setup react-navigation

--- App.js
+++ App.js
@@ -1,40 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react'
+import { View, Text } from 'react-native'
+import { createStackNavigator, createAppContainer } from 'react-navigation'
 
-import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  }
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-})
-
-type Props = {}
-export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Navigation workshop</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Navigation workshop</Text>
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
   },
 })
+
+export default createAppContainer(AppNavigator)
